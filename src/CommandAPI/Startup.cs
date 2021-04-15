45afe201090b2917ef07ecde65911d112be21763
sven haeldermans <sven.haeldermans@vvsg.be>
2021-04-15 22:33:59 +0200

    Added Model and Mock Repository

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,16 +7,25 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI
 {
-    public class Startup
+    public class NewBaseType
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        { services.AddControllers();
+        {
+            services.AddControllers();
+
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
+    }
+
+    public class Startup : NewBaseType
+    {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
